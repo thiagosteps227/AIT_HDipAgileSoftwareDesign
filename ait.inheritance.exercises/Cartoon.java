5a76190124bd747d5fc868b76381ea36c3da9c05
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-04 16:20:27 +0000

    Create Cartoon.java

---
+++ ait.inheritance.exercises/Cartoon.java
@@ -0,0 +1,33 @@
+package ait.inheritance.exercises;
+
+public class Cartoon {
+
+	public Cartoon() {
+		System.out.println("CARTOON constructor");
+	}
+	
+	public void sketch() {
+		System.out.println("CARTOON : sketch()");
+	}
+	
+	public void tomAndJerry() {
+		System.out.println("CARTOON: tomAndJerry()");
+	}
+	public static void main(String[] args) {
+		
+			
+	}
+	
+	public void sketchIt(Art a) {
+		sketch();
+	}
+	
+	public static void upcasting() {
+		
+	}
+	
+	public static void downcasting() {
+		
+	}
+
+}
