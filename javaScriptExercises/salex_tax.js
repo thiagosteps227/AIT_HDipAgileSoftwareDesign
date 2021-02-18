ce64dd3c7e96e1b14fe44c505e8947f95154b05e
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-18 09:18:17 +0000

    Update salex_tax.js

--- javaScriptExercises/salex_tax.js
+++ javaScriptExercises/salex_tax.js
@@ -1,15 +1,48 @@
 let $ = (id) => {
     return document.getElementById(id);
 } 
-
-
+let validationSubTotal = (number) =>{
+    if (isNaN(number) || number < 0 || number > 10000){
+        return console.error('The subtotal must be a positive number less then 10000');
+    }
+}
 function salesTaxCalculator(){
 
-    let subtotal = document.getElementById('subtotal').value;
-    let tax_rate = document.getElementById('tax_rate').value;
+    let subtotal = $('subtotal').value;
+    let tax_rate = $('tax_rate').value;
+    let isValid = true;
+
+    if (isNaN(subtotal) || subtotal < 0 || subtotal > 10000){
+        $("subtotal_message").firstChild.nodeValue = "Must be positive and less than 10000";
+        isValid = false;
+    } else {
+        $("subtotal_message").firstChild.nodeValue = "";
+        
+    }
+
+    if (isNaN(tax_rate) || tax_rate < 0 || tax_rate > 12){
+        $('tax_rate_message').firstChild.nodeValue = "Must be positive and less than 12";
+        isValid = false;
+    } else {
+        $('tax_rate_message').firstChild.nodeValue = "";
+    }
+
+    if (isValid === true){
     let salesTaxCalculated = parseFloat(subtotal) * (parseFloat(tax_rate)/100);
     let total = salesTaxCalculated + parseFloat(subtotal);
-
-    $('total').value = total;
+    
+    // display results
     $('sales_tax').value = salesTaxCalculated;
+    $('total').value = total.toFixed(2);
+    
+    // restore starting messages
+    $("subtotal_message").firstChild.nodeValue = "Enter order subtotal";
+    $("tax_rate_message").firstChild.nodeValue = "Enter sales tax rate (99.9)";
+
+    }
 }
+
+
+
+
+
