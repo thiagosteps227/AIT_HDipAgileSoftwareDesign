a2cef8dd343d98273ea9859afac9779a597d8893
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-19 10:14:35 +0000

    Update salex_tax.js

--- javaScriptExercises/salex_tax.js
+++ javaScriptExercises/salex_tax.js
@@ -6,12 +6,12 @@ let validationSubTotal = (number) =>{
         return console.error('The subtotal must be a positive number less then 10000');
     }
 }
-function salesTaxCalculator(){
+let calculateClick = () => {
 
-    let subtotal = $('subtotal').value;
-    let tax_rate = $('tax_rate').value;
+    let subtotal = parseFloat($('subtotal').value);
+    let tax_rate = parseFloat($('tax_rate').value);
     let isValid = true;
-
+    
     if (isNaN(subtotal) || subtotal < 0 || subtotal > 10000){
         $("subtotal_message").firstChild.nodeValue = "Must be positive and less than 10000";
         isValid = false;
@@ -27,9 +27,9 @@ function salesTaxCalculator(){
         $('tax_rate_message').firstChild.nodeValue = "";
     }
 
-    if (isValid === true){
-    let salesTaxCalculated = parseFloat(subtotal) * (parseFloat(tax_rate)/100);
-    let total = salesTaxCalculated + parseFloat(subtotal);
+    if (isValid){
+    let salesTaxCalculated = subtotal * (tax_rate/100);
+    let total = salesTaxCalculated + subtotal;
     
     // display results
     $('sales_tax').value = salesTaxCalculated;
@@ -40,8 +40,37 @@ function salesTaxCalculator(){
     $("tax_rate_message").firstChild.nodeValue = "Enter sales tax rate (99.9)";
 
     }
+    $('subtotal').focus();
 }
 
+let clearClick = () => {
+    $('subtotal').value = "";
+    $('tax_rate').value = "";
+    $('sales_tax').value = "";
+    $('total').value = "";
+    $('subtotal').focus();
+
+    // restore starting messages
+    $("subtotal_message").firstChild.nodeValue = "Enter order subtotal";
+    $("tax_rate_message").firstChild.nodeValue = "Enter sales tax rate (99.9)";
+
+}
+
+let subtotalClick = () => {
+    $('subtotal').value = "";
+}
+
+let taxRateClick = () => {
+    $('tax_rate').value = "";
+}
+
+window.onload = () => {
+    $('calculate').onclick = calculateClick;
+    $('clear').onclick = clearClick;
+    $('subtotal').onclick = subtotalClick;
+    $('tax_rate').onclick = taxRateClick;
+    $('subtotal').focus();
+}
 
 
 
