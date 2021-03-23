7f60251b38d8bfee47ddfa57b42835d9784eb806
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-03-23 14:56:24 +0000

    Add files via upload

---
+++ CatPoundMavenProject/Cat.java
@@ -0,0 +1,39 @@
+package ait.com.catpoundexercise;
+
+public class Cat {
+
+	private String name;
+	private int age;
+	private String color;
+	
+	public Cat() {
+
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	
+	
+}
